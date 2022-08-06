fc8bfb58379837b7e65335f1a863ee36337dc3b2
Joao Julio <joaopjt@gmail.com>
2022-08-05 22:26:36 -0300

    Writting events done

--- src/App.js
+++ src/App.js
@@ -7,22 +7,32 @@ import CangjieLibrary from './Odessa/index.js';
 import CangJieKeyBinding from './CangJieKeyBinding.json'
 import CangJieToogleKeyBinding from './CangJieKeyBinding.json'
 
-const handleKeyDown = (e) => {
-  console.log(e);
-};
-
 const CangJieKeyboard = () => {
   return <Keyboard mergeDisplay display={CangJieKeyBinding} physicalKeyboardHighlight />
 };
 
 function App() {
-  const [Text, setText] = useState("");
+  const library = CangjieLibrary.default;
+  let [Text, setText] = useState("");
   let runtime = 0;
   let phrase = "";
 
   const onChange = (e) => {
     e.preventDefault();
-    setText(CangjieLibrary.default[e.nativeEvent.data][0]);
+
+    if (e.nativeEvent.inputType === "deleteContentBackward") {
+      setText(Text.slice(0, Text.lenght - 1)[0]);
+    } else if (e.nativeEvent.inputType === "insertText") {
+      if (Text === "") {
+        setText(library[e.nativeEvent.data][0]);
+      } else {
+        let text = Text += library[e.nativeEvent.data][0];
+        setText(text);
+      }
+    }
+
+    
+    runtime++;
   };
 
   return (
