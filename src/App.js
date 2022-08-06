52f24278dd7b174741d5675b973283d333246a6b
Joao Julio <joaopjt@gmail.com>
2022-08-05 21:25:46 -0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,18 +1,35 @@
 import './App.css';
 import 'react-simple-keyboard/build/css/index.css';
 
-import Keyboard from 'react-simple-keyboard'
+import { useState } from 'react';
+import Keyboard from 'react-simple-keyboard';
+import CangjieLibrary from './Odessa/index.js';
 import CangJieKeyBinding from './CangJieKeyBinding.json'
+import CangJieToogleKeyBinding from './CangJieKeyBinding.json'
+
+const handleKeyDown = (e) => {
+  console.log(e);
+};
 
 const CangJieKeyboard = () => {
   return <Keyboard mergeDisplay display={CangJieKeyBinding} physicalKeyboardHighlight />
-}
+};
 
 function App() {
+  const [Text, setText] = useState("");
+  let runtime = 0;
+  let phrase = "";
+
+  const onChange = (e) => {
+    e.preventDefault();
+    setText(CangjieLibrary.default[e.nativeEvent.data][0]);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        
+        <textarea className="App-textbox" onChange={onChange} value={Text}>
+        </textarea>
       </header>
       <CangJieKeyboard />
     </div>
